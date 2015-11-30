e24601687d9b86e09907e135d5f14956bb83e0ec
Bruno Goncalves <bgoncalves@Glaurung.local>
2015-11-30 13:43:45 +0000

    picard, consensus rename, filter VCF

--- downloadAndBowtie.py
+++ downloadAndBowtie.py
@@ -129,7 +129,7 @@ def downloadAndBowtie(referencePath, run_id, target_dir, buildBowtie, picardJarP
 	
 	if numberFilesDowned==1:
 
-		command_line ="bowtie2 -k 2 --quiet --no-unal -x "+bowtieBuildFileName+" -U "+dir_with_gz+"/"+run_id+".fastq.gz --rg-id ENA --rg PL:illumina --rg SM:"+run_id+" --sensitive-local --threads "+ threads +" --met-file "+run_id+".bowtie_metrics.txt -S "+bowtie_output_file+" "
+		command_line ="bowtie2 -k 2 --quiet --no-unal -x "+bowtieBuildFileName+" -U "+dir_with_gz+"/"+run_id+".fastq.gz --rg-id ENA --rg PL:illumina --rg SM:"+run_id+" --sensitive-local --threads "+ str(threads) +" --met-file "+run_id+".bowtie_metrics.txt -S "+bowtie_output_file+" "
 
 
 		myoutput = open(bowtieLog, 'w')
@@ -139,7 +139,7 @@ def downloadAndBowtie(referencePath, run_id, target_dir, buildBowtie, picardJarP
 
 
 	elif numberFilesDowned==2:
-		command_line ="bowtie2 -k 2 --quiet --no-unal -x "+bowtieBuildFileName+" -1 "+dir_with_gz+"/"+run_id+"_1.fastq.gz -2 "+dir_with_gz+"/"+run_id+"_2.fastq.gz --rg-id ENA --rg PL:illumina --rg SM:"+run_id+" --sensitive-local --threads "+ threads +" --met-file "+run_id+".bowtie_metrics.txt -S "+bowtie_output_file+" "
+		command_line ="bowtie2 -k 2 --quiet --no-unal -x "+bowtieBuildFileName+" -1 "+dir_with_gz+"/"+run_id+"_1.fastq.gz -2 "+dir_with_gz+"/"+run_id+"_2.fastq.gz --rg-id ENA --rg PL:illumina --rg SM:"+run_id+" --sensitive-local --threads "+ str(threads) +" --met-file "+run_id+".bowtie_metrics.txt -S "+bowtie_output_file+" "
 		
 		myoutput = open(bowtieLog, 'w')
 		args = shlex.split(command_line)
