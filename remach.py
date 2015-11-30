b63f62771d0dfd9f8f36837564f80de6395f4ef9
Bruno Goncalves <bgoncalves@Glaurung.local>
2015-11-30 13:55:17 +0000

    picard, consensus rename, filter VCF

--- remach.py
+++ remach.py
@@ -62,7 +62,7 @@ def runReMaCh(args):
 			sequenceNames, sequenceMedObject, sequenceAndIndex = checkCoverage(sortedPath, args.cov)
 			alleleCalling(sortedPath, args.r, sequenceNames, args.gatk, run_id, args.qual, args.cov, args.mul, sequenceMedObject, sequenceAndIndex)
 
-			filesToRemove = glob.glob(os.path.join(args.t, run_id) + '/*.fastq.gaz')
+			filesToRemove = glob.glob(os.path.join(args.t, run_id) + '/*.fastq.gz')
 
 			for i in filesToRemove:
 				print i
