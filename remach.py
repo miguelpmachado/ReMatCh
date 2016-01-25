c2b06fee4d49dd14e17ba1e8dbac8279e8579ec1
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-01-25 15:19:55 +0000

    more than 3 files

--- remach.py
+++ remach.py
@@ -52,6 +52,7 @@ def runReMaCh(args):
 		platform=''
 	
 	ids_with_problems = open(os.path.join(args.t,'ids_with_problems.txt'), 'w')
+	logFile = open(os.path.join(args.t,'log_file.txt'), 'a')
 
 	with open(args.l, 'r') as run_ids:
 
@@ -95,8 +96,10 @@ def runReMaCh(args):
 
 
 				print "\n######\ndownloading and bowtieying\n######\n"
+				logFile.write("\n######\ndownloading and bowtieying\n######\n")
 				samFilePath, singOrPaired, numFilesDownloaded = downloadAndBowtie(args.r, run_id, args.t, buildBowtie, args.picard, args.threads)
 				print "\n######\ndownloaded and bowtied\n######\n"
+				logFile.write("\n######\ndownloaded and bowtied\n######\n")
 				
 				if numFilesDownloaded == 0:
 					ids_with_problems.write(run_id + '\n')
