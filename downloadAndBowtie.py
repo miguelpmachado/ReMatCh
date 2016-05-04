71c5efa77cf447932dce52dddaedf4c007fe8cdb
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-05-04 15:19:22 +0100

    change fastq_names

--- downloadAndBowtie.py
+++ downloadAndBowtie.py
@@ -137,7 +137,7 @@ def downloadAndBowtie(referencePath, run_id, target_dir, buildBowtie, picardJarP
 		print 'File '+ run_id+' already exists...' 
 		logFile.write('File '+ run_id+' already exists...' + '\n')
 
-	
+
 	
 	#download_ERR(run_id, target_dir)
 
@@ -163,7 +163,7 @@ def downloadAndBowtie(referencePath, run_id, target_dir, buildBowtie, picardJarP
 
 
 	elif numberFilesDowned==2:
-		command_line ="bowtie2 -k 2 --quiet --no-unal -x "+bowtieBuildFileName+" -1 "+dir_with_gz+"/"+run_id+"_R1_001.fastq.gz -2 "+dir_with_gz+"/"+run_id+"_R2_001.fastq.gz --rg-id ENA --rg SM:"+run_id+" --sensitive-local --threads "+ str(threads) +" --met-file "+ os.path.join(dir_with_gz, run_id+".bowtie_metrics.txt") + " -S "+bowtie_output_file+" "
+		command_line ="bowtie2 -k 2 --quiet --no-unal -x "+bowtieBuildFileName+" -1 "+dir_with_gz+"/"+run_id+"_1.fastq.gz -2 "+dir_with_gz+"/"+run_id+"_2.fastq.gz --rg-id ENA --rg SM:"+run_id+" --sensitive-local --threads "+ str(threads) +" --met-file "+ os.path.join(dir_with_gz, run_id+".bowtie_metrics.txt") + " -S "+bowtie_output_file+" "
 		
 		myoutput = open(bowtieLog, 'w')
 		args = shlex.split(command_line)
