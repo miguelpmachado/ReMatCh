c25cb6b88d4eaf09a50cdb7d72bea58fb6a68988
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-05-13 16:34:48 +0100

    mergeResults

--- mergeResults.py
+++ mergeResults.py
@@ -7,11 +7,9 @@ def mergeResults(workdir, sequenceCoverage):
 	#sampleList = os.listdir(workdir)
 	dirs = [d for d in os.listdir(workdir) if os.path.isdir(os.path.join(workdir, d))]
 	sampledict = {}
-	print dirs
 
 	for i in dirs:
 		sampleName = os.path.basename(i)
-		print os.path.join(workdir, sampleName, sampleName+'_mappingCheck.tab')
 		mappingFilePath = os.path.join(workdir, sampleName, 'rematch_results', sampleName+'_mappingCheck.tab')
 		if os.path.isfile(mappingFilePath):
 			with open(mappingFilePath, 'r') as csvfile:
